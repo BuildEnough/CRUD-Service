967bed33d04c825a325deffbf381e9ef30889450
BuildEnough <rhtmaehcl878@naver.com>
2023-11-16 19:22:40 +0900

    글 작성 처리

--- board/src/main/java/com/project/board/controller/BoardController.java
+++ board/src/main/java/com/project/board/controller/BoardController.java
@@ -1,15 +1,29 @@
 package com.project.board.controller;
 
+import com.project.board.entity.Board;
+import com.project.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
 
     @GetMapping("/board/write")
     public String boardWriteForm() {
 
         return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+
+        boardService.write(board);
+
+        return "";
     }
 }
