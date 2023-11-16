41b47cddc5866939081fb199f8e4002235cda816
BuildEnough <rhtmaehcl878@naver.com>
2023-11-16 22:07:52 +0900

    게시글 삭제

--- board/src/main/java/com/project/board/controller/BoardController.java
+++ board/src/main/java/com/project/board/controller/BoardController.java
@@ -42,4 +42,12 @@ public class BoardController {
         model.addAttribute("board", boardService.boardView(id));
         return "boardview";
     }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
 }
